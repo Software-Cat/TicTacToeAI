5d1ff7f2643b20065ecae76fda86443104be93ab
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2020-11-10 20:23:07 +1100

    implement tile placing

--- main.py
+++ main.py
@@ -1,18 +1,60 @@
 import tkinter as tk
+import enum
+import random
+from tkinter import Grid
 
 
-class Game:
+class Game(tk.Frame):
+    @enum.unique
+    class GridState(enum.Enum):
+        EMPTY = ""
+        O = "O"
+        X = "X"
+
+        def __init__(self, state: str) -> None:
+            self.state = state
+
+        def __str__(self) -> str:
+            return self.state
+
     def __init__(self, master=None):
+        super().__init__(master)
         self.master = master
-        self.grid = []
+        self.grid(row=0, column=0)
+        self.create_widgets()
+
+        self.turn = random.choice([self.GridState.X, self.GridState.O])
+
+    def create_widgets(self):
+        self.matrix = []
 
         for row in range(3):
             currentRow = []
             for column in range(3):
-                btn = tk.Button(text="", width=12, height=6)
-                btn.grid(row=row, column=column)
-                currentRow.append(btn)
-            self.grid.append(currentRow)
+                button = tk.Button()
+                button.config(
+                    width=3, height=1,
+                    text=self.GridState.EMPTY,
+                    command=lambda context=button: self.button_press_callback(
+                        context),
+                    font=("JetBrains Mono", 36, "bold")
+                )
+                button.grid(row=row, column=column)
+                currentRow.append(button)
+            self.matrix.append(currentRow)
+
+    def button_press_callback(self, context: tk.Button):
+        print("Button Pressed")
+        if self.GridState(context["text"]) == self.GridState.EMPTY:
+            context["text"] = self.turn
+            context["state"] = tk.DISABLED
+            self.switch_players()
+
+    def switch_players(self):
+        if self.turn is self.GridState.O:
+            self.turn = self.GridState.X
+        else:
+            self.turn = self.GridState.O
 
 
 if __name__ == "__main__":
