382e0f2ead6b79dbeec510426a2eea9bce33f723
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2020-11-10 17:29:05 +1100

    setup tkinter unittesting framework

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 import tkinter as tk
 
 
-root = tk.Tk()
-root.title("Tic Tac Toe")
+class Game:
+    def __init__(self, master=None):
+        self.master = master
+        self.grid = []
 
-button1 = tk.Button(root, text=" ", height=4, width=8)
-button1.grid(row=3, column=0)
+        for row in range(3):
+            currentRow = []
+            for column in range(3):
+                btn = tk.Button(text="", width=12, height=6)
+                btn.grid(row=row, column=column)
+                currentRow.append(btn)
+            self.grid.append(currentRow)
 
-button2 = tk.Button(root,  text=" ", height=4, width=8)
-button2.grid(row=3, column=1)
 
-button3 = tk.Button(root,  text=" ", height=4, width=8)
-button3.grid(row=3, column=2)
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('Tic Tac Toe')
+    root.resizable(False, False)
 
-button4 = tk.Button(root,  text=" ", height=4, width=8)
-button4.grid(row=4, column=0)
+    game = Game(root)
 
-button5 = tk.Button(root,  text=" ", height=4, width=8)
-button5.grid(row=4, column=1)
-
-button6 = tk.Button(root,  text=" ", height=4, width=8)
-button6.grid(row=4, column=2)
-
-button7 = tk.Button(root,  text=" ", height=4, width=8)
-button7.grid(row=5, column=0)
-
-button8 = tk.Button(root,  text=" ", height=4, width=8)
-button8.grid(row=5, column=1)
-
-button9 = tk.Button(root,  text=" ", height=4, width=8)
-button9.grid(row=5, column=2)
-
-tk.mainloop()
+    root.mainloop()
--- test.py
+++ test.py
@@ -1,6 +1,37 @@
 import unittest
+import tkinter as tk
+import _tkinter
+import main
 
 
-class Tests(unittest.TestCase):
-    def test_sanity(self):
-        self.assertEqual(1 + 1, 2)
+class TKinterTestCase(unittest.TestCase):
+    """Common methods used for every GUI test.
+    """
+
+    def setUp(self):
+        self.root = tk.Tk()
+        self.pump_events()
+
+    def tearDown(self):
+        if self.root:
+            self.root.destroy()
+            self.pump_events()
+
+    def pump_events(self):
+        while self.root.dooneevent(_tkinter.ALL_EVENTS | _tkinter.DONT_WAIT):
+            pass
+
+
+class TestButtons(TKinterTestCase):
+    def test_button_num(self):
+        game = main.Game(self.root)
+
+        self.pump_events()
+
+        self.assertEqual(len(game.grid), 3)
+        self.assertEqual(len(game.grid[0]), 3)
+        self.assertEqual(len(game.grid[1]), 3)
+        self.assertEqual(len(game.grid[2]), 3)
+
+    def test_button_press(self):
+        pass
